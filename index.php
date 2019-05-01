07252c9388d61a113afa3628d235e4f9ec7a6e48
dorge47 <chris_jandrade@yahoo.com>
2019-05-01 14:05:14 -0700

    PHP garbo

---
+++ index.php
@@ -0,0 +1,19 @@
+<?php
+    require('modules/header.php');
+
+    if(isset($_GET['page'])){
+        $page = $_GET['page'];
+        if(file_exists( "content/$page.php")){
+            include("content/$page.php");
+        }
+        else {
+            echo "<h1>404: You may be lost!</h1>";
+            //or include 404 content
+        }
+    }
+    else {
+        include("content/home.php");
+    }
+
+    require('modules/footer.php');
+?>
