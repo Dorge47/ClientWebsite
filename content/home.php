a98d80d7752addcbfb5e6ec7298562b93fc48a7e
CraftyBoss <talibabdulmaalik@gmail.com>
2019-05-09 15:31:16 -0700

    continued work on styling for home.php

--- content/home.php
+++ content/home.php
@@ -1,13 +1,17 @@
 <div class="mainCont">
+    <img src="img/placeholdbg.jpg" alt="" id="bgImg">
     <div class="centeredRow">
         <div class="row">
             <div class="txtCont">
-                <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.</p>
-                <a href="?page=services">Services Page</a>
-                <a href="?page=pricing">Pricing Page</a>
+                <h1>Header</h1><br>
+                <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.</p><br>
+                <div class="txtButtons">
+                    <a href="?page=services" align="center">Services Page</a>
+                    <a href="?page=pricing" align="center">Pricing Page</a>
+                </div>
             </div>
             <div class="googReview">
-                    <img src="http://www.placehold.it/500x500" alt="">
+                    <img src="http://www.placehold.it/600x500" alt="">
             </div>
         </div>
     </div>
