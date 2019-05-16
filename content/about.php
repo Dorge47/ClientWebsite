7015f83e80b476b1011f7555d2fda5be07ff6d48
dorge47 <chris_jandrade@yahoo.com>
2019-05-16 15:22:03 -0700

    Worked on about page

--- content/about.php
+++ content/about.php
@@ -1,6 +1,20 @@
-<div class="mainCont">
+<div class="about">
+    <div class="phone">
+        <h3 class="head">Contact</h3>
+        <a href="tel:+1-559-326-7015" class="purpleBtn head">Call now</a>
+        <p>(559) 326-7015</p>
+    </div>
+    <div class="address">
+        <h3 class="head">Address</h3>
+        <a href="https://www.google.com/maps/dir//1st+Choice+Computer+Services,+635+Barstow+Ave,+Clovis,+CA+93612/@36.8160863,-119.7033532,17z/data=!4m8!4m7!1m0!1m5!1m1!1s0x80945c7e8f9524fd:0xc77c772be11ae6f4!2m2!1d-119.7011645!2d36.8160863" class="purpleBtn head">Get Directions</a>
+        <div>
+            <p>635 Barstow Avenue</p>
+            <p>Clovis, California 93612</p>
+            <p>United States</p>
+        </div>
+    </div>
     <div class="hours">
-        <h3>Business Hours</h3>
+        <h3 class="head">Business Hours</h3>
         <p>Monday: 9:00 AM – 5:00 PM</p>
         <p>Tuesday: 9:00 AM – 5:00 PM</p>
         <p>Wednesday: 9:00 AM – 5:00 PM</p>
@@ -8,8 +22,5 @@
         <p>Friday: 9:00 AM – 1:00 PM</p>
         <p>Saturday: Closed</p>
         <p>Sunday: 9:00 AM – 1:00 PM</p>
-    </div>
-    <div class="address">
-        <a href="#" id="address"></a>
     </div>
 </div>
